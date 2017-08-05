ef36f99ccbc1a237466b759d8ce6e6f32962e32c
alroysh <cicak.jatuh02@gmail.com>
2017-08-06 01:28:59 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -219,7 +219,7 @@
 				$get_sub = array();
 				$aa =   array(
 								'type' => 'text',									
-								'text' => 'Wib  : '. $wib
+								'text' => 'Wib  : '. $wib \n 'Wita'
 							);
 				array_push($get_sub,$aa);	
 				$get_sub[] = array(
