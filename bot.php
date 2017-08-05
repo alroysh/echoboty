6198c57209660820c319e6b4a433804f9af8d203
alroysh <cicak.jatuh02@gmail.com>
2017-08-06 01:43:07 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -49,6 +49,19 @@
 		$random3 = (rand()%300);
 
 
+function CallLineGetName($access_token,$userId)
+{
+
+  $url = 'https://api.line.me/v2/bot/profile/'.$userId;
+  $headers = array('Authorization: Bearer ' .$access_token);
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  $result = curl_exec($ch);
+  curl_close($ch);
+  return $result;
+}
 if(!is_null($json['displayName']))
 {
 
