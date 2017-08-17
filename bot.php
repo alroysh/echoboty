9474fa969f1c0b7d84156e1cc4ac2f743c25658a
alroysh <cicak.jatuh02@gmail.com>
2017-08-18 00:33:34 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -50,6 +50,8 @@
 		$userx = $message['text'];
 		$data = explode(":", $userx);
 		$datac = "/ig:".$data[1]."";
+		$anu - explode("<tr class="head">", $userx);
+		$datab = "qwerty";
 
 function CallLineGetName($access_token,$userId)
 {
@@ -416,6 +418,20 @@ if (!is_null($datas['id']))
 									)
 								);
 			}
+		else if($pesan_datang==$datab)
+		{
+		$api = file_get_contents("http://propana.otoreport.com/harga.js.php");
+		$text3 = "Harga";
+		$balas = array(
+		'replyToken' => $replyToken,														
+		'messages' => array(
+				array(
+					'type' => 'text',					
+					'text' => $text3
+					)
+					)
+					);
+		}
 		else if($pesan_datang==$datac)
 				{
 				 $api_ig = file_get_contents("https://www.instagram.com/".$data[1]."/?__a=1");
