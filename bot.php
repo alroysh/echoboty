a6367067002e0125e46b15ff7c877d125f2341ff
alroysh <cicak.jatuh02@gmail.com>
2017-08-17 19:46:42 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -428,8 +428,8 @@ if (!is_null($datas['id']))
 				Id : ".$jss->user->id."
 				bio : ".$jss->user->biography."
 				Followers : ".$jss->user->followed_by->count."
-				Following : ".$jss->user->followers->count."
-				Post : ".$jss->user->mediia->count."
+				Following : ".$jss->user->follows>count."
+				Post : ".$jss->user->media->count."
 				Bio : ".$jss->user->biography."";
 				$balas = array(
 					'replyToken' => $replyToken,
